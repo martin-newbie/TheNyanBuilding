c9ed1b6623c1994c83dcec403d0078b043b782a7
martin-newbie <g.martin.min@gmail.com>
2022-07-09 16:18:29 +0900

    add load prefabs

--- Assets/Minkeunsub/Scripts/InGame/InGameManager.cs
+++ Assets/Minkeunsub/Scripts/InGame/InGameManager.cs
@@ -2,11 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CharacterType
+{
+    Bro,
+    Developer,
+    QualityAssureance,
+    ProductManager
+}
+
 public class InGameManager : Singleton<InGameManager>
 {
+    [Header("Characters")]
+    public Character Bro;
+    public Character[] Dev;
+    public Character[] QA;
+    public Character[] PM;
+
+    public void InitCharacters()
+    {
+        Bro = Resources.Load<Character>("Characters/Bro");
+        Dev = Resources.LoadAll<Character>("Characters/DEV");
+        QA = Resources.LoadAll<Character>("Characters/QA");
+        PM = Resources.LoadAll<Character>("Characters/PM");
+    }
 
 
+    public void SpawnCharacter(CharacterType type, int idx = 0)
+    {
 
+        Vector2 spawnPos = new Vector2();
+
+        switch (type)
+        {
+            case CharacterType.Bro:
+                SpawnCharacter(Bro, spawnPos);
+                break;
+            case CharacterType.Developer:
+                SpawnCharacter(Dev[idx], spawnPos);
+                break;
+            case CharacterType.QualityAssureance:
+                SpawnCharacter(QA[idx], spawnPos);
+                break;
+            case CharacterType.ProductManager:
+                SpawnCharacter(PM[idx], spawnPos);
+                break;
+        }
+    }
+
+    public Character SpawnCharacter(Character prefab, Vector2 pos)
+    {
+        Character spawnTmp = Instantiate(prefab, pos, Quaternion.identity);
+        return spawnTmp;
+    }
 }
 
 public interface ITouchAble
