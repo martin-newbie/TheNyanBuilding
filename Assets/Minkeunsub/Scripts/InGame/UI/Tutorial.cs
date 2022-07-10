66c063a1385f017ee5743f7b3062f8632fc298a1
martin-newbie <g.martin.min@gmail.com>
2022-07-10 17:03:37 +0900

    add tutorial

--- Assets/Minkeunsub/Scripts/InGame/UI/Tutorial.cs
+++ Assets/Minkeunsub/Scripts/InGame/UI/Tutorial.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StartCartoon : MonoBehaviour
+public class Tutorial : MonoBehaviour
 {
 
     public Image CartoonImg;
+    public Button HelpButton;
+    public Image TutorialImg;
     bool isActive;
 
     private void Start()
     {
         InGameManager.Instance.isUImoving = true;
+        CartoonImg.gameObject.SetActive(true);
     }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0) && !isActive)
+        if (Input.GetMouseButtonDown(0) && !isActive)
         {
             StartCoroutine(FadeOut(0.5f));
             isActive = true;
         }
+
+        if (TutorialImg.gameObject.activeSelf && !trigger && Input.GetMouseButtonDown(0))
+        {
+            trigger = true;
+        }
     }
+
+    bool trigger;
 
     IEnumerator FadeOut(float duration)
     {
@@ -37,8 +47,23 @@ public class StartCartoon : MonoBehaviour
             yield return null;
         }
 
+        CartoonImg.gameObject.SetActive(false);
+
+        while (!trigger) yield return null;
+        trigger = false;
+        HelpButton.gameObject.SetActive(false);
+
+        TutorialImg.gameObject.SetActive(true);
+
+        while (!trigger) yield return null;
+
+        TutorialImg.gameObject.SetActive(false);
         InGameManager.Instance.isUImoving = false;
-        gameObject.SetActive(false);
+    }
+
+    public void HelpButtonFunc()
+    {
+        trigger = true;
     }
 
 }
